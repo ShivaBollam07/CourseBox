d462759ada5017352ae76474c7f86d4ae5003d54
shivabollam07 <shivabollam07@gmail.com>
2023-12-28 18:24:20 +0530

    false-true

--- App.js
+++ App.js
@@ -11,10 +11,11 @@ import ResourceOptions from './Screens/ResourceOptions';
 const Stack = createStackNavigator();
 
 export default function App() {
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: true }} />
         <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Stack.Screen
           name="CourseDetail"
